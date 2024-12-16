e7a5033cd10e9daa8fe479a7bf694c7f7a64eab8
Hyejin <hyejin.jeon940120@gmail.com>
2024-12-16 15:58:46 +0900

    setting 2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734321414',resources: [
+    cacheName: 'chirpy-1734332184',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
