6df9b9a6ac088972d2f9255cf6f33fba857860e3
Hyejin <hyejin.jeon940120@gmail.com>
2024-12-16 16:46:33 +0900

    google sitemap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1734332184',resources: [
+    cacheName: 'chirpy-1734334993',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
